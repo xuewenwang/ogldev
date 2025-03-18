e7a2845bc47f95441714f3839c702a7d7c3e9788
xuewenwang <xuewenwang@gzshixiang.com>
2025-03-18 17:45:52 +0800

    remove same apt get lib
    Signed-off-by: xuewenwang <xuewenwang@gzshixiang.com>

--- install_requirements_ubuntu.sh
+++ install_requirements_ubuntu.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-apt install g++ make freeglut3 freeglut3-dev libglew-dev libglew2.2 libmagick++-dev libassimp-dev libglfw3 libglfw3-dev libmeshoptimizer-dev libmeshoptimizer1d vulkan-tools vulkan-validationlayers vulkan-validationlayers-dev glslang-dev glslang-tools
+#apt install g++ make freeglut3 freeglut3-dev libglew-dev libglew2.2 libmagick++-dev libassimp-dev libglfw3 libglfw3-dev libmeshoptimizer-dev libmeshoptimizer1d vulkan-tools vulkan-validationlayers vulkan-validationlayers-dev glslang-dev glslang-tools
+apt install g++ make freeglut3 freeglut3-dev libglew-dev libassimp-dev libglfw3 libglfw3-dev libmeshoptimizer-dev libmeshoptimizer1d vulkan-tools vulkan-validationlayers vulkan-validationlayers-dev glslang-dev glslang-tools
+#apt install g++ make freeglut3 freeglut3-dev libglew-dev libassimp-dev libglfw3 libglfw3-dev vulkan-tools vulkan-validationlayers vulkan-validationlayers-dev glslang-dev glslang-tools
